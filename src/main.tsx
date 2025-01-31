39e05e2186b73a4851525384a89cef179713c8b5
imasterlion <imvincentshen@gmail.com>
2025-01-31 21:28:42 +0800

    Adjust CSS import order

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
+import './index.css'  // 移到 CssBaseline 之后
 import App from './App'
 import { theme } from './theme'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
