31f85916987197de990f670cefbaafc88e352812
imasterlion <imvincentshen@gmail.com>
2025-01-31 18:08:33 +0800

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// 改用 HashRouter
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import UploadRecipe from './pages/UploadRecipe';
